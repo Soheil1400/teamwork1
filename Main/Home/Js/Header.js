5b9030e8c03229c697d640557c9135ca8a1f9ab0
majid <majid.ir90@yahoo.com>
2021-10-11 15:52:38 +0330

    dynamic header

--- Main/Home/Js/Header.js
+++ Main/Home/Js/Header.js
@@ -4,3 +4,19 @@ function showMenu() {
         list.style.display= "none"
     } else list.style.display= "block"
 }
+$(function () {
+    let list = document.getElementById("header");
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            list.classList.add("sticky");
+        } else {
+            list.classList.remove("sticky")
+        }
+        if ($(this).scrollTop() > 100) {
+            list.classList.add("inView");
+        } else {
+            list.classList.remove("inView")
+        }
+    })
+});
